296b94c404b8de19a3db7de598b567f7514e16d5
nexiuse <yogawahyu0308@gmail.com>
2026-04-08 12:35:23 +0700

    Update NexHub_Loader.lua

--- NexHub_Loader.lua
+++ NexHub_Loader.lua
@@ -45,10 +45,12 @@ local GameList = {
     -- FREE GAMES
     { name = "FishZar",           placeIds = {121442629947656},                    type = "free",    scriptUrl = "https://raw.githubusercontent.com/nexiuse/NexHub/main/NexHubFishZar.lua" },
     { name = "Fish God",          placeIds = {121500015379301},                    type = "free",    scriptUrl = "https://raw.githubusercontent.com/nexiuse/NexHub/main/NexHubFishGod.lua" },
+    { name = "Survive The Apocalypse",  placeIds = {90148635862803},               type = "free",    scriptUrl = "https://raw.githubusercontent.com/nexiuse/NexHub/main/NexHubSurviveTheApocalypse.lua" },
 
     -- PREMIUM GAMES (Butuh Key)
     { name = "Blox Fruits",       placeIds = {2753915549, 4442272183, 7449423635}, type = "premium", scriptUrl = "https://raw.githubusercontent.com/nexiuse/NexHub/main/BloxFruitsLuxv'SHubXNex_protected.lua" },
     { name = "Violence District", placeIds = {93978595733734},                    type = "premium", scriptUrl = "https://raw.githubusercontent.com/nexiuse/NexHub/main/NexHubVD.lua" },
+    { name = "Sailor Piece",      placeIds = {77747658251236},                        type = "premium", scriptUrl = "https://raw.githubusercontent.com/nexiuse/NexHub/refs/heads/main/NexHubSailorPiece.lua" },
 }
 
 -- ============================================
